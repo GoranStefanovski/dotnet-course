0a7cb39a0904d87c4de96283019f927deee0b3db
goran-isource <goran.s@isource.com.mk>
2025-10-10 15:50:39 +0200

    DotnetAPI Course Exercises

---
+++ DotnetAPI/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    DataContextDapper _dapper;
+
+    public UserController(IConfiguration config)
+    {
+        _dapper = new DataContextDapper(config);
+    }
+
+    [HttpGet("TestConnection")]
+    public DateTime TestConnection()
+    {
+        return _dapper.LoadDataSingle<DateTime>("SELECT NOW()");
+    }
+
+    [HttpGet("GetUser/{testValue}")]
+    public string[] GetUser(string testValue)
+    {
+        string[] responseArray = new string[] {
+        "test1",
+        "test2",
+        testValue
+       };
+        return responseArray;
+    }
+
+    [HttpGet("GetUsers")]
+    public IEnumerable<User> GetUsers()
+    {
+        string sql = @"
+            SELECT [UserId],
+                   [FirstName],
+                   [LastName],
+                   [Email],
+                   [Gender],
+                   [Active]
+            FROM TutorialAppSchema.Users";
+        IEnumerable<User> users = _dapper.LoadData<User>(sql);
+        return users;
+    }
+
+    [HttpGet("GetSingleUser/{userId}")]
+    public User GetSingleUser(int userId)
+    {
+        string sql = @"
+            SELECT [UserId],
+                   [FirstName],
+                   [LastName],
+                   [Email],
+                   [Gender],
+                   [Active]
+            FROM TutorialAppSchema.Users
+            WHERE UserId = " + userId.ToString();
+        User user = _dapper.LoadDataSingle<User>(sql);
+        return user;
+    }
+}
